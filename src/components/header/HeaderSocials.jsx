fd9541ef382327de9c84b5dbaf5fb766ba6f0e9e
Dharun1504 <149801112+Dharun1504@users.noreply.github.com>
2025-02-27 16:50:37 +0530

    Update HeaderSocials.jsx

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ const HeaderSocials = () => {
     <div className="header__socials">
       <a href="https://www.linkedin.com/in/dharunraj-p" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/Dharun1504" target="_blank" rel="noreferrer" ><FaGithub /></a>
-      <a href="https://www.instagram.com/dharun10" target="_blank" rel="noreferrer" ><AiFillInstagram /></a>
+      <a href="https://www.instagram.com/_.dharun._10?igsh=MWJydHE3NTExa2QzeA==" target="_blank" rel="noreferrer" ><AiFillInstagram /></a>
     </div>
   )
 }
